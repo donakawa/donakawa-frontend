e1479f131fe9290c489070a4e52cfa49ca74e7c3
hagyeong <hagyeong13@naver.com>
2026-01-27 21:22:44 +0900

    ✨ Feat: 위시리스트_공통컴포넌트 퍼블리싱 (#15)

--- src/pages/WishlistPage/components/wishlistPanel.tsx
+++ src/pages/WishlistPage/components/wishlistPanel.tsx
@@ -14,19 +14,18 @@ export default function WishlistPanel({
   return (
     <section
       className={[
-        'flex-1 min-h-0 flex flex-col',
-        
-        editMode ? 'bg-[#EFEFEF]' : 'bg-[color:var(--color-secondary-100)]',
+        'w-[375px] h-[570px] mt-[221px] mx-auto',
+        'flex flex-col',
+        editMode ? 'bg-[color:var(--color-gray-100)]' : 'bg-[color:var(--color-secondary-100)]',
         'rounded-t-[20px]',
         'shadow-[0_0_4px_rgba(0,0,0,0.25)]',
         'overflow-hidden',
-        'relative',
       ].join(' ')}
     >
-      <div className="px-[16px] pt-[16px] flex-1 flex flex-col min-h-0">
+      <div className="flex-1 min-h-0 flex flex-col gap-[16px] p-[20px] pb-[16px]">
         {children}
       </div>
-      
+
       {bottomPaddingPx > 0 && (
         <div style={{ height: bottomPaddingPx }} className="shrink-0" />
       )}
